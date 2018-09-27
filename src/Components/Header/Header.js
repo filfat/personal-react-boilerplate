64bf32fcf8d874fa65a61124a50811fcf925747a
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2018-09-27 09:39:23 +0200

    Initial prismic..

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,83 @@
 import React, { Component } from 'react';
 
+import LinkResolver from '../../Utils/LinkResolver';
+import API from '../../Utils/API';
+
 import './Header.css';
 
 class Header extends Component {
-    
+    state = {}
+
+    constructor (props) {
+        super(props);
+
+        this.handleScroll = this.handleScroll.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    componentDidMount() {
+        let self = this;
+
+        const Prismic = API.Prismic;
+        Prismic.then(api => {
+            api.getSingle('header').then((document) => {
+                self.setState({ doc: document });
+            });
+        }).catch(ex => {
+            self.setState({ doc: {offline: true} });
+            console.log(ex);
+        });
+
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll () {
+        if (!this.state.scrolled) {
+            if (window.scrollY >= 15)
+                this.setState({ scrolled : true});
+        } else if (this.state.scrolled)
+            if (window.scrollY < 15)
+                this.setState({ scrolled : false});
+    }
+    handleToggle () {
+        this.setState({
+            prev_scroll: window.pageYOffset,
+            menu_open: !this.state.menu_open
+        });
+    }
+
     render () {
+        if (!this.state.doc) return null;
+        const doc = this.state.doc;
+
+        if (!this.state.links) {
+            let links = [];
+            for (let i = 0; i < doc.data.body.length; i++) {
+                let link = doc.data.body[i];
+
+                if(link.slice_type === 'link')
+                    links.push(<a key={i} className="link" href={LinkResolver(link.primary.link)}>{link.primary.title}</a>);
+                else if (link.slice_type === 'button')
+                    links.push(<a key={i} className="link button" href={LinkResolver(link.primary.link)}>{link.primary.title}</a>);
+            }
+
+            this.setState({ links: links });
+        }
+
         return (
-            <header>
+            <header className={this.state.scrolled ? 'scrolled' : ''}>
+                <div className="container">
+                    <a className={this.state.menu_open ? 'logo force-white' : 'logo'} href="/">Header</a>
+                    <div className={this.state.menu_open ? 'toggle-button force-white' : 'toggle-button'} onClick={this.handleToggle}><i className="material-icons">menu</i></div>
+
+                    <nav className={this.state.menu_open ? 'open' : ''}>
+                        {this.state.links}
+                    </nav>
+                </div>
             </header>
         );
     }
--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,25 +1,12 @@
-import axios from 'axios';
-import Logger from './logger';
+import Prismic from 'prismic-javascript';
+
+const API_DOMAIN    = 'example';
+const API_ENDPOINT  = `https://${API_DOMAIN}.cdn.prismic.io/api/v2`;
+// const API_TOKEN  = '';
 
 class API {
     constructor () {
-        this.axios = axios.create({
-            baseURL: 'https://localhost:5000/',
-            timeout: 5000
-        });
-    }
-
-    Get (path) {
-        Logger.Info('API', 'Calling "' + path + '"');
-
-        const call = this.axios.get(path);
-        call.then((res) => {
-            Logger.Info('API', 'Response from "' + path + '":');
-            Logger.Object(res);
-        }).catch((err) => {
-            Logger.Error('API', 'Response from "' + path + '": "' + err + '"');
-        });
-        return call;
+        this.Prismic = Prismic.getApi(API_ENDPOINT);
     }
 }
 
